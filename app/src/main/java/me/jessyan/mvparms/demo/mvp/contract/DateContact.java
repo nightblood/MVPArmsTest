53ecad336f6ed727c6689d87134835daa9e4229e
zlf <165374900@163.com>
2017-01-18 16:38:28 +0800

    add DateFragment

---
+++ app/src/main/java/me/jessyan/mvparms/demo/mvp/contract/DateContact.java
@@ -0,0 +1,16 @@
+package me.jessyan.mvparms.demo.mvp.contract;
+
+import me.jessyan.mvparms.demo.mvp.BaseView;
+import me.jessyan.mvparms.demo.mvp.IModel;
+
+/**
+ * Created by Administrator on 2017/1/18 0018.
+ */
+public interface DateContact {
+    interface View extends BaseView {
+
+    }
+    interface Model extends IModel {
+
+    }
+}
---
+++ app/src/main/java/me/jessyan/mvparms/demo/mvp/ui/fragment/news/DateFragment.java
@@ -0,0 +1,64 @@
+package me.jessyan.mvparms.demo.mvp.ui.fragment.news;
+
+import android.content.Intent;
+import android.view.View;
+
+import me.jessyan.mvparms.demo.base.BaseFragment;
+import me.jessyan.mvparms.demo.mvp.contract.DateContact;
+import me.jessyan.mvparms.demo.mvp.presenter.DatePresenter;
+
+/**
+ * Created by Administrator on 2017/1/18 0018.
+ */
+public class DateFragment extends BaseFragment<DatePresenter> implements DateContact.View{
+
+    @Override
+    protected void ComponentInject() {
+
+    }
+
+    @Override
+    protected View initView() {
+        return null;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    public void showMessage(String message) {
+
+    }
+
+    @Override
+    public void loadSuccess(Object o) {
+
+    }
+
+    @Override
+    public void loadFail() {
+
+    }
+
+    @Override
+    public void launchActivity(Intent intent) {
+
+    }
+
+    @Override
+    public void killMyself() {
+
+    }
+}
