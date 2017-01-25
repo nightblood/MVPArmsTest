446745cb441fd237b58ae7e0318a2da8a6d9afc7
zlf <165374900@163.com>
2017-01-25 16:42:46 +0800

    Retrofit 使用 ScalarConverterFactory 的转换器

---
+++ app/src/main/java/me/jessyan/mvparms/demo/mvp/model/entity/LoginEntity.java
@@ -0,0 +1,7 @@
+package me.jessyan.mvparms.demo.mvp.model.entity;
+
+/**
+ * Created by Administrator on 2017/1/25 0025.
+ */
+public class LoginEntity {
+}
---
+++ app/src/main/java/me/jessyan/mvparms/demo/mvp/view/IRecommendFrgView.java
@@ -0,0 +1,7 @@
+package me.jessyan.mvparms.demo.mvp.view;
+
+/**
+ * Created by Administrator on 2017/1/25 0025.
+ */
+public class IRecommendFrgView {
+}
